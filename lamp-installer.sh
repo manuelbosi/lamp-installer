5d40e11adbdccfd35416899e703266092232cd2c
Manuel Bosi <manuelbosi57@gmail.com>
2021-11-27 18:51:50 +0100

    add phpmyadmin installation and configuration

--- lamp-installer.sh
+++ lamp-installer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#### GLOBAL ####
+ip="$(ifconfig | grep -Eo '(addr:)?([0-9]*\.){3}[0-9]*' | head -1)"
+
 #### COLOR ####
 INFO=$(tput setaf 6)
 QUESTION=$(tput setaf 3)
@@ -26,7 +29,14 @@ logger () {
 }
 
 remove_old_installations () {
-    apt-get remove --purge -y 'apache2-*' 'mysql-*' 'php-*' > /dev/null
+    apt-get clean > /dev/null
+    apt-get purge -y 'apache2*' 'mysql*' 'php*' 'phpmyadmin*' > /dev/null
+    # rm -rf /etc/apache2
+    # rm -rf /etc/mysql
+    # rm -rf /var/lib/mysql
+    # rm -rf /usr/share/phpMyAdmin* > /dev/null
+    # rm -rf /usr/share/phpmyadmin* > /dev/null
+    # rm -f /etc/apache2/conf-available/phpmyadmin.conf > /dev/null
 }
 
 update_repositories() {
@@ -58,6 +68,14 @@ flush_privileges () {
     logger success "Privileges have been reloaded"
 }
 
+start_service () {
+    service "$1" start > /dev/null
+}
+
+restart_service () {
+    service "$1" restart > /dev/null
+}
+
 ### MAIN ###
 
 # Check if the script is running as root
@@ -79,7 +97,10 @@ fi
 # Clean the system
 logger info "Cleaning the system from previous installations"
 remove_old_installations
+update_repositories
 logger success "System cleanup completed"
+
+exit 
 
 # Install dependencies
 apt-get install -y software-properties-common > /dev/null
@@ -92,7 +113,7 @@ logger info "Installing apache2"
 apt-get install -y apache2 > /dev/null
 logger success "Apache has been installed"
 logger info "Starting apache2 service"
-service apache2 start > /dev/null
+start_service apache2
 exit_status=$?
 
 # Check exit status and log response
@@ -109,7 +130,7 @@ apt-get install -y mysql-server mysql-client > /dev/null
 logger success "Mysql has been installed"
 logger info "Starting mysql service"
 usermod -d /var/lib/mysql/ mysql > /dev/null
-service mysql start > /dev/null
+start_service mysql
 exit_status=$?
 
 # Check exit status and log response
@@ -164,15 +185,55 @@ logger success "PHP has been installed"
 logger info "Restarting apache2 service"
 rm -rf /var/www/html/*
 echo "<?php phpinfo(); ?>" > /var/www/html/index.php
-service apache2 restart > /dev/null
+restart_service apache2
 exit_status=$?
 
 # Check exit status and log response
 if [ "$exit_status" -eq 0 ]
 then
     logger success "Apache2 service restarted"
-    ip="$(ifconfig | grep -Eo '(addr:)?([0-9]*\.){3}[0-9]*' | head -1)"
     logger info "Check if everything works fine here: $ip"
 else
     logger error "Cannot start apache2"
 fi
+
+# PHPMYADMIN Installation
+logger info "Installing phpMyAdmin dependencies"
+apt-get install -y php"$PHP_VERSION"-mbstring php"$PHP_VERSION"-gettext php"$PHP_VERSION"-mysqli php"$PHP_VERSION"-xml > /dev/null
+logger info "Downloading phpMyAdmin"
+cd /usr/share/
+
+# Download phpMyAdmin
+phpMyAdminArchive=phpMyAdmin-"$PHPMYADMIN_VERSION"-all-languages.tar.gz
+wget -q https://files.phpmyadmin.net/phpMyAdmin/"$PHPMYADMIN_VERSION"/"$phpMyAdminArchive"
+exit_status=$?
+
+# Check exit status and log response
+if [ "$exit_status" -ne 0 ]
+then
+    logger error "Error while downloading phpMyAdmin"
+    exit
+fi
+
+# Install
+logger info "Installing phpMyAdmin"
+tar -xf "$phpMyAdminArchive" > /dev/null
+mv phpMyAdmin-"$PHPMYADMIN_VERSION"-all-languages phpmyadmin > /dev/null
+rm -rf phpMyAdmin-"$PHPMYADMIN_VERSION"-all-languages
+rm -rf "$phpMyAdminArchive"
+chown -R www-data: phpmyadmin
+chmod -R 744 phpmyadmin
+
+logger success "phpMyAdmin has been installed"
+
+phpmyadmin_conf="$OLDPWD"/phpmyadmin.conf
+if [ -f "$phpmyadmin_conf" ]
+then
+    logger info "Updating phpMyAdmin configuration"
+    # sed -i "s/IP_ADDRESS/$ip/" "$phpmyadmin_conf"
+    cp "$phpmyadmin_conf" /etc/apache2/conf-available/
+fi
+
+a2enconf phpmyadmin > /dev/null
+logger success "phpMyAdmin configuration has beed updated"
+restart_service apache2
